ad4747deb49f7369acae93eb4c82f9b474876413
Marco Piermatei <piermateimarco54@gmail.com>
2025-12-15 15:01:08 -0300

    delete account page from settings

--- src/components/settings/SessionSection/index.jsx
+++ src/components/settings/SessionSection/index.jsx
@@ -1,13 +1,12 @@
-// src/components/settings/SessionSection/index.jsx - REFACTORIZADO
+// src/components/settings/SessionSection/index.jsx
 
 import React from 'react';
 import ***REMOVED*** LogOut ***REMOVED*** from 'lucide-react';
-import ***REMOVED*** useNavigate ***REMOVED*** from 'react-router-dom';
+import ***REMOVED*** useNavigate, Link ***REMOVED*** from 'react-router-dom';
 import ***REMOVED*** useAuth ***REMOVED*** from '../../../contexts/AuthContext';
 import ***REMOVED*** useThemeColors ***REMOVED*** from '../../../hooks/useThemeColors';
 import SettingsSection from '../SettingsSection';
 import Button from '../../ui/Button';
-import Flex from '../../ui/Flex';
 
 const SessionSection = (***REMOVED*** onError, className = '' ***REMOVED***) => ***REMOVED***
   const ***REMOVED*** logout ***REMOVED*** = useAuth();
@@ -25,7 +24,7 @@ const SessionSection = (***REMOVED*** onError, className = '' ***REMOVED***) => 
 
   return (
     <SettingsSection icon=***REMOVED***LogOut***REMOVED*** title="Sesión" className=***REMOVED***className***REMOVED***>
-      <Flex variant="center" className="flex-1">
+      <div className="flex flex-col items-center justify-center flex-1 w-full gap-3 mt-4">
         <Button
           onClick=***REMOVED***handleLogout***REMOVED***
           variant="outline"
@@ -35,7 +34,14 @@ const SessionSection = (***REMOVED*** onError, className = '' ***REMOVED***) => 
         >
           Cerrar sesión
         </Button>
-      </Flex>
+
+        <Link 
+          to="/delete-account"
+          className="text-xs text-gray-400 hover:text-gray-600 hover:underline transition-colors mt-1"
+        >
+          Eliminar cuenta
+        </Link>
+      </div>
     </SettingsSection>
   );
 ***REMOVED***;
